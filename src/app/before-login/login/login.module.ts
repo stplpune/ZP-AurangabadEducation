576648195427ab41f28b96bce12ac963bcd1a110
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2023-09-15 16:34:32 +0530

    login page designed

--- src/app/before-login/login/login.module.ts
+++ src/app/before-login/login/login.module.ts
@@ -3,7 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -11,7 +14,11 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class LoginModule { }
