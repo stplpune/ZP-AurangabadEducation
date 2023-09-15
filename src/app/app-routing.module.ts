3addaab132cb5bfa4dec402b9ba37f5581cfc9fe
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2023-09-15 17:52:04 +0530

    inner and outer pages routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicComponent } from './layout/public/public.component';
+import { SecureComponent } from './layout/secure/secure.component';
 
-const routes: Routes = [{ path: 'login', loadChildren: () => import('./before-login/login/login.module').then(m => m.LoginModule) }];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', component: PublicComponent, loadChildren: () => import('./layout/public/public.module').then(m => m.PublicModule) },
+  { path: '', component: SecureComponent, loadChildren: () => import('./layout/secure/secure.module').then(m => m.SecureModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
