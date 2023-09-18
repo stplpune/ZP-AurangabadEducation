7940aa26720b16494832e21e73a371e5a2035224
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2023-09-18 16:42:05 +0530

    sidebar close button added in sidebar

--- src/app/layout/secure/sidebar/sidebar.component.ts
+++ src/app/layout/secure/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WebStorageService } from 'src/app/services/web-storage.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
+  constructor(private webStorage: WebStorageService) {
 
+  }
+
+  onCloseSidebar() {
+    this.webStorage.setSidebarState(!this.webStorage.getSidebarState());
+  }
 }
