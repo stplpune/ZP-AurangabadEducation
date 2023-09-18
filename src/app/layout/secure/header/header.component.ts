9d2fe24fefc1a40b67e9c076e305a1993c792c1c
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2023-09-18 16:41:52 +0530

    sidebar open close buttons for mobile added in header

--- src/app/layout/secure/header/header.component.ts
+++ src/app/layout/secure/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WebStorageService } from 'src/app/services/web-storage.service';
 
 @Component({
   selector: 'app-header',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(private webStorage: WebStorageService) {
+
+  }
+
+
+  onClickSidebar() {
+    this.webStorage.setSidebarState(!this.webStorage.getSidebarState());
+
+  }
 }
