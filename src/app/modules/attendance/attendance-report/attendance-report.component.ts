b68b4c1c3081e1194ead3dca094ea4d68d17946b
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2023-09-20 18:27:56 +0530

    - create attendance report category component

---
+++ src/app/modules/attendance/attendance-report/attendance-report.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attendance-report',
+  templateUrl: './attendance-report.component.html',
+  styleUrls: ['./attendance-report.component.scss']
+})
+export class AttendanceReportComponent {
+
+}
