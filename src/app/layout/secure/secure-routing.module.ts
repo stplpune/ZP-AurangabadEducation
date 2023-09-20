5d81674feaf52af19f33ac6d32ab26fcecae0ccf
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2023-09-20 17:50:07 +0530

    create all master routing in secure routing   component

--- src/app/layout/secure/secure-routing.module.ts
+++ src/app/layout/secure/secure-routing.module.ts
@@ -5,6 +5,13 @@ import { SecureComponent } from './secure.component';
 const routes: Routes = [
   { path: '', component: SecureComponent },
   { path: 'dashboard', loadChildren: () => import('../../modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'designation-master', loadChildren: () => import('../../modules/masters/designation-master/designation-master.module').then(m => m.DesignationMasterModule) },
+  { path: 'school-registration', loadChildren: () => import('../../modules/masters/school-registration/school-registration.module').then(m => m.SchoolRegistrationModule) },
+  { path: 'teacher-registration', loadChildren: () => import('../../modules/masters/teacher-registration/teacher-registration.module').then(m => m.TeacherRegistrationModule) },
+  { path: 'student-registration', loadChildren: () => import('../../modules/masters/student-registration/student-registration.module').then(m => m.StudentRegistrationModule) },
+  { path: 'office-user-registration', loadChildren: () => import('../../modules/masters/office-user-registration/office-user-registration.module').then(m => m.OfficeUserRegistrationModule) },
+  { path: 'other-registration', loadChildren: () => import('../../modules/masters/other-registration/other-registration.module').then(m => m.OtherRegistrationModule) },
+
 ];
 
 @NgModule({
