8132ca1049e434d236af16726f466adf4671d7cc
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2023-09-21 15:34:25 +0530

    add angular modules in dsignation master modules file

--- src/app/modules/masters/designation-master/designation-master.module.ts
+++ src/app/modules/masters/designation-master/designation-master.module.ts
@@ -3,6 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { DesignationMasterRoutingModule } from './designation-master-routing.module';
 import { DesignationMasterComponent } from './designation-master.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import {MatTableModule} from '@angular/material/table';
 
 
 @NgModule({
@@ -11,7 +22,19 @@ import { DesignationMasterComponent } from './designation-master.component';
   ],
   imports: [
     CommonModule,
-    DesignationMasterRoutingModule
+    DesignationMasterRoutingModule,
+    MatCardModule,
+    MatSelectModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatAutocompleteModule,
+    MatTooltipModule,
+    MatTableModule
   ]
 })
 export class DesignationMasterModule { }
