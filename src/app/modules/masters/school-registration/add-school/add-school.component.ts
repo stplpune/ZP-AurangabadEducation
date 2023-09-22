a06df058fb3836bc7a678e9c1415f2f38d44e4ff
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2023-09-22 15:41:41 +0530

    working on add school form

---
+++ src/app/modules/masters/school-registration/add-school/add-school.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-school',
+  templateUrl: './add-school.component.html',
+  styleUrls: ['./add-school.component.scss']
+})
+export class AddSchoolComponent {
+
+}
