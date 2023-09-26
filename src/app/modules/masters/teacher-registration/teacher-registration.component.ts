3c37b281c1b1d7c4eb633e103d18b4291fa860c2
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2023-09-26 18:01:58 +0530

    teacher registration form completed with design

--- src/app/modules/masters/teacher-registration/teacher-registration.component.ts
+++ src/app/modules/masters/teacher-registration/teacher-registration.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddTeacherComponent } from './add-teacher/add-teacher.component';
+import {ThemePalette} from '@angular/material/core';
+export interface PeriodicElement {
+  srno: any;
+  TeacherNum: any;
+  Teacher: any;
+  Mobile: any;
+  Email: any;
+  Taluka: any;
+  Cluster: any;
+  Unblock: any;
+  Action: any;
+}
 
+const ELEMENT_DATA: PeriodicElement[] = [
+  {srno: 1, TeacherNum: 'Teacher', Teacher: 'School', Mobile: 'Head Master', Email:'Anjangaon Bari', Taluka:'Jaoli',Cluster:'AMBI',Unblock: '', Action: 'H'},
+  {srno: 2, TeacherNum: 'Head Master', Teacher: 'Kendra', Mobile: 'Cluster Resource Person', Email:'Anjangaon Bari',Taluka:'Jaoli',Cluster:'AMBI',Unblock: '',   Action: 'H'},
+  {srno: 3, TeacherNum: 'IED Teacher', Teacher: 'Taluka', Mobile: 'Block Resource Person', Email:'Anjangaon Bari',Taluka:'Jaoli',Cluster:'AMBI',Unblock: '',   Action: 'H'},
+];
 @Component({
   selector: 'app-teacher-registration',
   templateUrl: './teacher-registration.component.html',
   styleUrls: ['./teacher-registration.component.scss']
 })
 export class TeacherRegistrationComponent {
+  constructor(public dialog: MatDialog,){
 
+  }
+  displayedColumns: string[] = ['srno', 'TeacherNum', 'Teacher', 'Mobile', 'Email', 'Taluka','Cluster','Unblock','Action'];
+  dataSource = ELEMENT_DATA;
+  AddTeacher(data?: any) {
+    const dialogRef = this.dialog.open(AddTeacherComponent, {
+      width: '800px',
+      data: data,
+      disableClose: true
+        });
+
+    dialogRef.afterClosed().subscribe(() => {
+      dialogRef.close();
+    })
+}
+color: ThemePalette = 'accent';
+  checked = false;
+  disabled = false;
 }
--- src/app/modules/masters/teacher-registration/teacher-registration.module.ts
+++ src/app/modules/masters/teacher-registration/teacher-registration.module.ts
@@ -3,15 +3,48 @@ import { CommonModule } from '@angular/common';
 
 import { TeacherRegistrationRoutingModule } from './teacher-registration-routing.module';
 import { TeacherRegistrationComponent } from './teacher-registration.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { AddTeacherComponent } from './add-teacher/add-teacher.component';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 @NgModule({
   declarations: [
-    TeacherRegistrationComponent
+    TeacherRegistrationComponent,
+    AddTeacherComponent
   ],
   imports: [
     CommonModule,
-    TeacherRegistrationRoutingModule
+    TeacherRegistrationRoutingModule,
+    MatCardModule,
+    MatSelectModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatAutocompleteModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSlideToggleModule
+
   ]
 })
 export class TeacherRegistrationModule { }
