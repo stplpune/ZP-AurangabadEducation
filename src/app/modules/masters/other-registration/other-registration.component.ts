04b45be5f6b887e7d84690f2753085fb8881dfb9
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2023-09-27 12:05:43 +0530

    Ohter registration design completed

--- src/app/modules/masters/other-registration/other-registration.component.ts
+++ src/app/modules/masters/other-registration/other-registration.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddOtherRegistrationComponent } from './add-other-registration/add-other-registration.component';
+export interface PeriodicElement {
+  srno: any;
+  Name: any;
+  Contact: any;
+  Email: any;
+  Action: any;
+}
 
+const ELEMENT_DATA: PeriodicElement[] = [
+  {srno: 1, Name: 'Ngo Demo', Contact:'9878788778',Email:'test@gmail.com',Action: ''},
+  {srno: 2, Name: 'Ramajogayya Shastry',Contact:'9878788778',Email:'ramajogayya@gmail.com',Action: ''},
+  {srno: 3, Name: 'Prajapati Naidu',Contact:'9878788778',Email:'parajapti@gmail.com',Action: ''},
+];
 @Component({
   selector: 'app-other-registration',
   templateUrl: './other-registration.component.html',
   styleUrls: ['./other-registration.component.scss']
 })
 export class OtherRegistrationComponent {
+  constructor(public dialog: MatDialog,){
 
+  }
+  displayedColumns: string[] = ['srno', 'Name','Contact','Email','Action'];
+  dataSource = ELEMENT_DATA;
+  AddOtherRegistration(data?: any) {
+    const dialogRef = this.dialog.open(AddOtherRegistrationComponent, {
+      width: '800px',
+      data: data,
+      disableClose: true
+        });
+
+    dialogRef.afterClosed().subscribe(() => {
+      dialogRef.close();
+    })
+}
 }
--- src/app/modules/masters/other-registration/other-registration.module.ts
+++ src/app/modules/masters/other-registration/other-registration.module.ts
@@ -3,15 +3,50 @@ import { CommonModule } from '@angular/common';
 
 import { OtherRegistrationRoutingModule } from './other-registration-routing.module';
 import { OtherRegistrationComponent } from './other-registration.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { AddOtherRegistrationComponent } from './add-other-registration/add-other-registration.component';
 
 
 @NgModule({
   declarations: [
-    OtherRegistrationComponent
+    OtherRegistrationComponent,
+    AddOtherRegistrationComponent
   ],
   imports: [
     CommonModule,
-    OtherRegistrationRoutingModule
+    OtherRegistrationRoutingModule,
+    MatCardModule,
+    MatSelectModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatAutocompleteModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSlideToggleModule,
+    MatRadioModule
   ]
 })
 export class OtherRegistrationModule { }
