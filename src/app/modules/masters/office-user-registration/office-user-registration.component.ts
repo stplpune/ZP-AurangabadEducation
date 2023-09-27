f32a5b917d9facb7e2549a353aa4312165cb02dc
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2023-09-27 12:05:00 +0530

    Office user registration design completed

--- src/app/modules/masters/office-user-registration/office-user-registration.component.ts
+++ src/app/modules/masters/office-user-registration/office-user-registration.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddUserComponent } from './add-user/add-user.component';
+export interface PeriodicElement {
+  srno: any;
+  Name: any;
+  User: any;
+  Level: any;
+  Designation: any;
+  Contact: any;
+  Block: any;
+  Action: any;
+}
 
+const ELEMENT_DATA: PeriodicElement[] = [
+  {srno: 1, Name: 'Ngo Demo', User: 'NGO', Level: 'Kendra', Designation:'Kendra Pramukh', Contact:'9878788778', Block:'', Action: ''},
+  {srno: 2, Name: 'Ramajogayya Shastry', User: 'Staff', Level: 'School', Designation:'Block Resource Person', Contact:'9878788778', Block:'',  Action: ''},
+  {srno: 3, Name: 'Prajapati Naidu', User: 'NGO', Level: 'District', Designation:'CEO', Contact:'9878788778',  Block:'',  Action: ''},
+];
 @Component({
   selector: 'app-office-user-registration',
   templateUrl: './office-user-registration.component.html',
   styleUrls: ['./office-user-registration.component.scss']
 })
 export class OfficeUserRegistrationComponent {
+  constructor(public dialog: MatDialog,){
 
+  }
+  displayedColumns: string[] = ['srno', 'Name', 'User', 'Level', 'Designation', 'Contact', 'Block','Action'];
+  dataSource = ELEMENT_DATA;
+  AddUser(data?: any) {
+    const dialogRef = this.dialog.open(AddUserComponent, {
+      width: '600px',
+      data: data,
+      disableClose: true
+        });
+
+    dialogRef.afterClosed().subscribe(() => {
+      dialogRef.close();
+    })
+}
+  color: ThemePalette = 'accent';
+  checked = false;
+  disabled = false;
 }
--- src/app/modules/masters/office-user-registration/office-user-registration.module.ts
+++ src/app/modules/masters/office-user-registration/office-user-registration.module.ts
@@ -3,15 +3,50 @@ import { CommonModule } from '@angular/common';
 
 import { OfficeUserRegistrationRoutingModule } from './office-user-registration-routing.module';
 import { OfficeUserRegistrationComponent } from './office-user-registration.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { AddUserComponent } from './add-user/add-user.component';
+import {MatRadioModule} from '@angular/material/radio';
 
 
 @NgModule({
   declarations: [
-    OfficeUserRegistrationComponent
+    OfficeUserRegistrationComponent,
+    AddUserComponent
   ],
   imports: [
     CommonModule,
-    OfficeUserRegistrationRoutingModule
+    OfficeUserRegistrationRoutingModule,
+    MatCardModule,
+    MatSelectModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatAutocompleteModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSlideToggleModule,
+    MatRadioModule
   ]
 })
 export class OfficeUserRegistrationModule { }
