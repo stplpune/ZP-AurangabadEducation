396704f48aafee9b2bcc9d043429cf06979c5a11
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2023-09-27 12:07:12 +0530

    Student registration form design Completed

--- src/app/modules/masters/student-registration/student-registration.component.ts
+++ src/app/modules/masters/student-registration/student-registration.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+export interface PeriodicElement {
+  srno: any;
+  Student: any;
+  Standard: any;
+  Mobile: any;
+  Gender: any;
+  Action: any;
+}
 
+const ELEMENT_DATA: PeriodicElement[] = [
+  {srno: 1, Student: 'Yogi', Standard: '1st', Mobile: '7687856567', Gender:'Male', Action: ''},
+  {srno: 2, Student: 'Rihan', Standard: '2nd', Mobile: '9089877665', Gender:'Male', Action: ''},
+  {srno: 3, Student: 'Nisha', Standard: '3rd', Mobile: '78899887778', Gender:'Male',  Action: ''},
+];
 @Component({
   selector: 'app-student-registration',
   templateUrl: './student-registration.component.html',
   styleUrls: ['./student-registration.component.scss']
 })
 export class StudentRegistrationComponent {
+  constructor(public dialog: MatDialog,){
 
+  }
+  displayedColumns: string[] = ['srno', 'Student', 'Standard', 'Mobile', 'Gender', 'Action'];
+  dataSource = ELEMENT_DATA;
 }
--- src/app/modules/masters/student-registration/student-registration.module.ts
+++ src/app/modules/masters/student-registration/student-registration.module.ts
@@ -3,6 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { StudentRegistrationRoutingModule } from './student-registration-routing.module';
 import { StudentRegistrationComponent } from './student-registration.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 @NgModule({
@@ -11,7 +26,23 @@ import { StudentRegistrationComponent } from './student-registration.component';
   ],
   imports: [
     CommonModule,
-    StudentRegistrationRoutingModule
+    StudentRegistrationRoutingModule,
+    MatCardModule,
+    MatSelectModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatAutocompleteModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSlideToggleModule
   ]
 })
 export class StudentRegistrationModule { }
