662b0a2f9e56cac680a564f843534b7baf93200a
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2023-10-03 16:24:15 +0530

    - Inward item design completed component

--- src/app/modules/asset-management/inward-item/inward-item.component.ts
+++ src/app/modules/asset-management/inward-item/inward-item.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddInwardItemsComponent } from './add-inward-items/add-inward-items.component';
+export interface PeriodicElement {
+  srno: any;
+  Category: any;
+  SubCategory: any;
+  Item: any;
+  Units: any;
+  PurchaseDate: any;
+  Price: any;
+  Remark: any;
+  Action: any;
+}
 
+const ELEMENT_DATA: PeriodicElement[] = [
+  {srno: 1,Category: 'drone', SubCategory: 'Camera', Item: 'fan',Units:'100',PurchaseDate:'13-09-2023',Price:'10',Remark:'Yes',Action:''},
+  {srno: 2, Category: 'Books', SubCategory: 'history gh', Item: 'Thailand Saturday',Units:'100',PurchaseDate:'03-09-2023',Price:'10',Remark:'Yes',Action:''},
+  {srno: 3, Category: 'Irons', SubCategory: 'irons table', Item: 'drinkf',Units:'100',PurchaseDate:'23-09-2023',Price:'10',Remark:'Yes',Action:''},
+];
 @Component({
   selector: 'app-inward-item',
   templateUrl: './inward-item.component.html',
   styleUrls: ['./inward-item.component.scss']
 })
 export class InwardItemComponent {
+  constructor(public dialog: MatDialog,){
 
+  }
+  displayedColumns: string[] = ['srno', 'Category', 'SubCategory', 'Item','Units','PurchaseDate','Price','Remark','Action'];
+  dataSource = ELEMENT_DATA;
+  AddInwardItems(data?: any) {
+    const dialogRef = this.dialog.open(AddInwardItemsComponent, {
+      width: '650px',
+      data: data,
+      disableClose: true
+        });
+
+    dialogRef.afterClosed().subscribe(() => {
+      dialogRef.close();
+    })
+}
 }
--- src/app/modules/asset-management/inward-item/inward-item.module.ts
+++ src/app/modules/asset-management/inward-item/inward-item.module.ts
@@ -3,15 +3,44 @@ import { CommonModule } from '@angular/common';
 
 import { InwardItemRoutingModule } from './inward-item-routing.module';
 import { InwardItemComponent } from './inward-item.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { AddInwardItemsComponent } from './add-inward-items/add-inward-items.component';
 
 
 @NgModule({
   declarations: [
-    InwardItemComponent
+    InwardItemComponent,
+    AddInwardItemsComponent
   ],
   imports: [
     CommonModule,
-    InwardItemRoutingModule
+    InwardItemRoutingModule,
+    MatCardModule,
+    MatSelectModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatAutocompleteModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ]
 })
 export class InwardItemModule { }
