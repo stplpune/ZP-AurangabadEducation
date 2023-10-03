37692f255df7c45c04d2232cb040265e3891c78d
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2023-10-03 15:15:31 +0530

    - sub category design completed component

--- src/app/modules/asset-management/sub-category/sub-category.component.ts
+++ src/app/modules/asset-management/sub-category/sub-category.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+export interface PeriodicElement {
+  srno: any;
+  Category : any;
+  SubCategory :any;
+  Action: any;
+}
 
+const ELEMENT_DATA: PeriodicElement[] = [
+  {srno: 1, Category : 'drone', SubCategory:'Camera',Action: 'H'},
+  {srno: 2, Category : 'Goldust', SubCategory:'Gram',Action: 'H'},
+  {srno: 3, Category : 'Plastic', SubCategory:'Plastic Chair',Action: 'H'},
+];
 @Component({
   selector: 'app-sub-category',
   templateUrl: './sub-category.component.html',
   styleUrls: ['./sub-category.component.scss']
 })
 export class SubCategoryComponent {
-
+  displayedColumns: string[] = ['srno', 'Category', 'SubCategory','Action'];
+  dataSource = ELEMENT_DATA;
 }
