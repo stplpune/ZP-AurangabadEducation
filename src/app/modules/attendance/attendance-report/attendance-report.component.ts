b6a493bd0fdfdd2f98caf6810fa80d7acd8816fa
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2023-10-03 18:52:44 +0530

    attendance report design completed

--- src/app/modules/attendance/attendance-report/attendance-report.component.ts
+++ src/app/modules/attendance/attendance-report/attendance-report.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+export interface PeriodicElement {
+  srno: any;
+  Teacher : any;
+  TeacherName: any;
+  MobileNo: any;
+  PresentDays: any;
+  AbsentDays: any;
+  TotalHoliday: any;
+  TotalWeek: any;
+  Manual: any;
+  Submitted: any;
+  ManualApproved: any;
+}
 
+const ELEMENT_DATA: PeriodicElement[] = [
+  {srno: 1,Teacher : 'drone', TeacherName: 'Camera', MobileNo: 'fan',PresentDays:'100',AbsentDays:'13-09-2023',TotalHoliday:'10',TotalWeek:'javed',Manual:'Yes',Submitted:'-',ManualApproved: '-'},
+  {srno: 2, Teacher : 'Books', TeacherName: 'history gh', MobileNo: 'Thailand Saturday',PresentDays:'100',AbsentDays:'03-09-2023',TotalHoliday:'10',TotalWeek:'javed',Manual:'Yes',Submitted:'-',ManualApproved: '-'},
+  {srno: 3, Teacher : 'Irons', TeacherName: 'irons table', MobileNo: 'drinkf',PresentDays:'100',AbsentDays:'23-09-2023',TotalHoliday:'10',TotalWeek:'javed',Manual:'Yes',Submitted:'-',ManualApproved: '-'},
+];
 @Component({
   selector: 'app-attendance-report',
   templateUrl: './attendance-report.component.html',
   styleUrls: ['./attendance-report.component.scss']
 })
 export class AttendanceReportComponent {
-
+  displayedColumns: string[] = ['srno', 'Teacher ', 'TeacherName', 'MobileNo','PresentDays','AbsentDays','TotalHoliday','TotalWeek','Manual','Submitted','ManualApproved'];
+  dataSource = ELEMENT_DATA;
 }
--- src/app/modules/attendance/attendance-report/attendance-report.module.ts
+++ src/app/modules/attendance/attendance-report/attendance-report.module.ts
@@ -3,6 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { AttendanceReportRoutingModule } from './attendance-report-routing.module';
 import { AttendanceReportComponent } from './attendance-report.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 
 @NgModule({
@@ -11,7 +24,21 @@ import { AttendanceReportComponent } from './attendance-report.component';
   ],
   imports: [
     CommonModule,
-    AttendanceReportRoutingModule
+    AttendanceReportRoutingModule,
+    MatCardModule,
+    MatSelectModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatAutocompleteModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ]
 })
 export class AttendanceReportModule { }
