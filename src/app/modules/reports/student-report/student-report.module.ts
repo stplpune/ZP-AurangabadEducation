0d1dbf96d5a0a22e8e5b6100f14cd8e1f4613758
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2023-10-03 11:39:38 +0530

    Student report form design Completed

--- src/app/modules/reports/student-report/student-report.module.ts
+++ src/app/modules/reports/student-report/student-report.module.ts
@@ -3,6 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { StudentReportRoutingModule } from './student-report-routing.module';
 import { StudentReportComponent } from './student-report.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 @NgModule({
@@ -11,7 +27,24 @@ import { StudentReportComponent } from './student-report.component';
   ],
   imports: [
     CommonModule,
-    StudentReportRoutingModule
+    StudentReportRoutingModule,
+    MatCardModule,
+    MatSelectModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatAutocompleteModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSlideToggleModule,
+    MatRadioModule
   ]
 })
 export class StudentReportModule { }
