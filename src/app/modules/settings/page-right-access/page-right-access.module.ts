cdba314dfd9e988824e01c3298dbc565b9ee2d07
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2023-10-03 12:37:50 +0530

    page right access form design completed

--- src/app/modules/settings/page-right-access/page-right-access.module.ts
+++ src/app/modules/settings/page-right-access/page-right-access.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {MatCheckboxModule} from '@angular/material/checkbox';
 import { PageRightAccessRoutingModule } from './page-right-access-routing.module';
 import { PageRightAccessComponent } from './page-right-access.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 
 @NgModule({
@@ -11,7 +24,22 @@ import { PageRightAccessComponent } from './page-right-access.component';
   ],
   imports: [
     CommonModule,
-    PageRightAccessRoutingModule
+    PageRightAccessRoutingModule,
+    MatCardModule,
+    MatSelectModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatAutocompleteModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatCheckboxModule
   ]
 })
 export class PageRightAccessModule { }
