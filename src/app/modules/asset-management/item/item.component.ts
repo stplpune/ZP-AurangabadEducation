03297eb2fc3fa6ba8d8f0db32a885d4375082b60
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2023-10-03 15:14:22 +0530

    - item form design completed and create  add item component

--- src/app/modules/asset-management/item/item.component.ts
+++ src/app/modules/asset-management/item/item.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { AddItemComponent } from './add-item/add-item.component';
+import { MatDialog } from '@angular/material/dialog';
+export interface PeriodicElement {
+  srno: any;
+  Category: any;
+  SubCategory: any;
+  Item: any;
+  Description: any;
+  Action: any;
+}
 
+const ELEMENT_DATA: PeriodicElement[] = [
+  {srno: 1,Category: 'drone', SubCategory: 'Camera', Item: 'fan',Description:'Mineral',Action:''},
+  {srno: 2, Category: 'Books', SubCategory: 'history gh', Item: 'Thailand Saturday',Description:'added derta',Action:''},
+  {srno: 3, Category: 'Irons', SubCategory: 'irons table', Item: 'drinkf',Description:'string',Action:''},
+];
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.scss']
 })
 export class ItemComponent {
+  constructor(public dialog: MatDialog,){
 
+  }
+  displayedColumns: string[] = ['srno', 'Category', 'SubCategory', 'Item','Description','Action'];
+  dataSource = ELEMENT_DATA;
+  AddItem(data?: any) {
+    const dialogRef = this.dialog.open(AddItemComponent, {
+      width: '500px',
+      data: data,
+      disableClose: true
+        });
+
+    dialogRef.afterClosed().subscribe(() => {
+      dialogRef.close();
+    })
+}
 }
--- src/app/modules/asset-management/item/item.module.ts
+++ src/app/modules/asset-management/item/item.module.ts
@@ -3,15 +3,44 @@ import { CommonModule } from '@angular/common';
 
 import { ItemRoutingModule } from './item-routing.module';
 import { ItemComponent } from './item.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { AddItemComponent } from './add-item/add-item.component';
 
 
 @NgModule({
   declarations: [
-    ItemComponent
+    ItemComponent,
+    AddItemComponent
   ],
   imports: [
     CommonModule,
-    ItemRoutingModule
+    ItemRoutingModule,
+    MatCardModule,
+    MatSelectModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatAutocompleteModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ]
 })
 export class ItemModule { }
