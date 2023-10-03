733da995c02572e1cac384c94c65a1bafd4955b6
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2023-10-03 16:22:43 +0530

    button in file upload design changed

--- src/app/modules/masters/school-registration/school-registration.component.ts
+++ src/app/modules/masters/school-registration/school-registration.component.ts
@@ -12,9 +12,9 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {srno: 1, UDISE:'12343', School: 'Teacher', Taluka: 'School', Kendra: 'Head Master', Village:'Anjangaon Bari', Action: 'H'},
-  {srno: 2, UDISE:'22321',School: 'Head Master', Taluka: 'Kendra', Kendra: 'Cluster Resource Person', Village:'Anjangaon Bari',  Action: 'H'},
-  {srno: 3, UDISE:'12122',School: 'IED Teacher', Taluka: 'Taluka', Kendra: 'Block Resource Person', Village:'Anjangaon Bari',  Action: 'H'},
+  { srno: 1, UDISE: '12343', School: 'Teacher', Taluka: 'School', Kendra: 'Head Master', Village: 'Anjangaon Bari', Action: 'H' },
+  { srno: 2, UDISE: '22321', School: 'Head Master', Taluka: 'Kendra', Kendra: 'Cluster Resource Person', Village: 'Anjangaon Bari', Action: 'H' },
+  { srno: 3, UDISE: '12122', School: 'IED Teacher', Taluka: 'Taluka', Kendra: 'Block Resource Person', Village: 'Anjangaon Bari', Action: 'H' },
 ];
 @Component({
   selector: 'app-school-registration',
@@ -22,20 +22,20 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./school-registration.component.scss']
 })
 export class SchoolRegistrationComponent {
-  constructor(public dialog: MatDialog,){
+  constructor(public dialog: MatDialog,) {
 
   }
-  displayedColumns: string[] = ['srno', 'UDISE','School', 'Taluka', 'Kendra', 'Village', 'Action'];
+  displayedColumns: string[] = ['srno', 'UDISE', 'School', 'Taluka', 'Kendra', 'Village', 'Action'];
   dataSource = ELEMENT_DATA;
   AddSchool(data?: any) {
     const dialogRef = this.dialog.open(AddSchoolComponent, {
       width: '800px',
       data: data,
       disableClose: true
-        });
+    });
 
     dialogRef.afterClosed().subscribe(() => {
       dialogRef.close();
     })
-}
+  }
 }
