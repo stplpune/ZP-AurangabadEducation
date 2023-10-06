6b34a6aff066b64dea7b1a71b49078f24f4cf995
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2023-10-06 13:24:40 +0530

    added matdialog and redirect change password form in header component

--- src/app/layout/secure/header/header.component.ts
+++ src/app/layout/secure/header/header.component.ts
@@ -1,20 +1,29 @@
 import { Component } from '@angular/core';
 import { WebStorageService } from 'src/app/services/web-storage.service';
+import { ChangePasswordComponent } from 'src/app/modules/profile/change-password/change-password.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-header',
+  standalone:true,
+  imports:[MatDialogModule, MatButtonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
 
-  constructor(private webStorage: WebStorageService) {
+  constructor(private webStorage: WebStorageService, public dialog:MatDialog) {
 
   }
 
 
   onClickSidebar() {
     this.webStorage.setSidebarState(!this.webStorage.getSidebarState());
-
   }
+  OnchangePasswordDialog(){
+    this.dialog.open(ChangePasswordComponent,{
+      width:'600px'
+        });
+}
 }
