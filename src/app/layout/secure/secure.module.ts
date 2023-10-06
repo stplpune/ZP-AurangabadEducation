d1e36905236a87f423c772a23500272828f34766
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2023-10-06 13:16:31 +0530

    added header component in secure module file

--- src/app/layout/secure/secure.module.ts
+++ src/app/layout/secure/secure.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { SecureRoutingModule } from './secure-routing.module';
 import { SecureComponent } from './secure.component';
 import { HeaderComponent } from './header/header.component';
@@ -13,7 +12,6 @@ import { MatButtonModule } from '@angular/material/button';
 @NgModule({
   declarations: [
     SecureComponent,
-    HeaderComponent,
     FooterComponent,
     SidebarComponent
   ],
@@ -21,7 +19,8 @@ import { MatButtonModule } from '@angular/material/button';
     CommonModule,
     SecureRoutingModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    HeaderComponent,
   ]
 })
 export class SecureModule { }
