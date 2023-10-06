ded8e52675d98d12b48593a1cc146a6719d082d2
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2023-10-06 13:15:55 +0530

    added my-profile page routing in secure module file

--- src/app/layout/secure/secure-routing.module.ts
+++ src/app/layout/secure/secure-routing.module.ts
@@ -31,6 +31,7 @@ const routes: Routes = [
   { path: 'assessment-crateria', loadChildren: () => import('../../modules/settings/assessment-crateria/assessment-crateria.module').then(m => m.AssessmentCrateriaModule) },
   { path: 'add-student', loadChildren: () => import('../../modules/masters/student-registration/add-student/add-student.module').then(m => m.AddStudentModule) },
   { path: 'view-stock-report', loadChildren: () => import('../../modules/asset-management/store-stock-report/view-stock-report/view-stock-report.module').then(m => m.ViewStockReportModule) },
+  { path: 'my-profile', loadChildren: () => import('../../modules/profile/my-profile/my-profile.module').then(m => m.MyProfileModule) },
 
 ];
 
