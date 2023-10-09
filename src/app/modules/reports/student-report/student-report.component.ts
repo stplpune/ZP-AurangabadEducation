24e0d60a512332114eb3bc56b69aed6825c850df
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2023-10-09 18:42:02 +0530

    student report list page redesigned

--- src/app/modules/reports/student-report/student-report.component.ts
+++ src/app/modules/reports/student-report/student-report.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 export interface PeriodicElement {
   srno: any;
   District: any;
@@ -15,9 +16,9 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {srno: 1, District: 'Satara', Taluka: 'Jaoli', Center: 'AMBI', SchoolCode:'1234', SchoolName: 'New english schools division 1', FullName: 'Ronit Sharma', StudentGender: 'Male', Standard: '1st', Exam: 'Primary Test', Assessment: 'Demo', Management: 'Test'},
-  {srno: 2, District: 'Satara', Taluka: 'Jaoli', Center: 'AMBI', SchoolCode:'2345', SchoolName: 'New english schools division 1', FullName: 'Gunjan Maheshwaree', StudentGender: 'Female', Standard: '3rd', Exam: 'Base Line', Assessment: 'Test', Management: 'Test'},
-  {srno: 3, District: 'Satara', Taluka: 'Jaoli', Center: 'AMBI', SchoolCode:'5456', SchoolName: 'New english schools division 1', FullName: 'Vidya Singh ', StudentGender: 'Female', Standard: '2nd', Exam: 'Mid Line', Assessment: 'Demo', Management: 'Test'},
+  { srno: 1, District: 'Satara', Taluka: 'Jaoli', Center: 'AMBI', SchoolCode: '1234', SchoolName: 'New english schools division 1', FullName: 'Ronit Sharma', StudentGender: 'Male', Standard: '1st', Exam: 'Primary Test', Assessment: 'Demo', Management: 'Test' },
+  { srno: 2, District: 'Satara', Taluka: 'Jaoli', Center: 'AMBI', SchoolCode: '2345', SchoolName: 'New english schools division 1', FullName: 'Gunjan Maheshwaree', StudentGender: 'Female', Standard: '3rd', Exam: 'Base Line', Assessment: 'Test', Management: 'Test' },
+  { srno: 3, District: 'Satara', Taluka: 'Jaoli', Center: 'AMBI', SchoolCode: '5456', SchoolName: 'New english schools division 1', FullName: 'Vidya Singh ', StudentGender: 'Female', Standard: '2nd', Exam: 'Mid Line', Assessment: 'Demo', Management: 'Test' },
 ];
 @Component({
   selector: 'app-student-report',
@@ -25,7 +26,11 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./student-report.component.scss']
 })
 export class StudentReportComponent {
- 
-  displayedColumns: string[] = ['srno', 'District', 'Taluka', 'Center', 'SchoolCode', 'SchoolName','FullName','StudentGender','Standard','Exam','Assessment','Management'];
+  range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null),
+  });
+
+  displayedColumns: string[] = ['srno', 'District', 'Taluka', 'Center', 'SchoolCode', 'SchoolName', 'FullName', 'StudentGender', 'Standard', 'Exam', 'Assessment', 'Management'];
   dataSource = ELEMENT_DATA;
 }
