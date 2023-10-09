b1bc3a1f99d6b59441351b13414c9dde77116903
Kajaldudhe <kajal.dudhe@shauryatechnosoft.com>
2023-10-09 17:00:06 +0530

    assessment createria form design completed

--- src/app/modules/settings/assessment-crateria/assessment-crateria.component.ts
+++ src/app/modules/settings/assessment-crateria/assessment-crateria.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddAssessmentCrateriaComponent } from './add-assessment-crateria/add-assessment-crateria.component';
+import { Router } from '@angular/router';
+export interface PeriodicElement {
+  srno: any;
+  District: any;
+  Standard: any;
+  Subject:any;
+  QuestionType: any;
+  MultipleOption: any;
+  Action: any;
+}
 
+const ELEMENT_DATA: PeriodicElement[] = [
+  { srno: 1, District: '12343', Standard: 'Teacher', Subject:'Pune',QuestionType: 'School', MultipleOption: 'Head Master',Action: 'H' },
+  { srno: 2, District: '22321', Standard: 'Head Master',Subject:'Pune', QuestionType: 'Kendra', MultipleOption: 'Cluster Resource Person',Action: 'H' },
+  { srno: 3, District: '12122', Standard: 'IED Teacher',Subject:'Pune', QuestionType: 'Taluka', MultipleOption: 'Block Resource Person', Action: 'H' },
+];
 @Component({
   selector: 'app-assessment-crateria',
   templateUrl: './assessment-crateria.component.html',
   styleUrls: ['./assessment-crateria.component.scss']
 })
 export class AssessmentCrateriaComponent {
+  activeModal: any;
+  success: any
+  constructor(public dialog: MatDialog,
+    private router: Router,) {
 
+  }
+  displayedColumns: string[] = ['srno', 'District', 'Standard','Subject', 'QuestionType', 'MultipleOption', 'Action'];
+  dataSource = ELEMENT_DATA;
+  AddAssessmentCrateria(data?: any) {
+    const dialogRef = this.dialog.open(AddAssessmentCrateriaComponent, {
+      width: '800px',
+      data: data,
+      disableClose:false    });
+    dialogRef.afterClosed().subscribe(() => {
+      dialogRef.close();
+    })
+  }
+  closeModal() { this.activeModal.close( this.success ); }
 }
--- src/app/modules/settings/assessment-crateria/assessment-crateria.module.ts
+++ src/app/modules/settings/assessment-crateria/assessment-crateria.module.ts
@@ -3,15 +3,44 @@ import { CommonModule } from '@angular/common';
 
 import { AssessmentCrateriaRoutingModule } from './assessment-crateria-routing.module';
 import { AssessmentCrateriaComponent } from './assessment-crateria.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { AddAssessmentCrateriaComponent } from './add-assessment-crateria/add-assessment-crateria.component';
 
 
 @NgModule({
   declarations: [
-    AssessmentCrateriaComponent
+    AssessmentCrateriaComponent,
+    AddAssessmentCrateriaComponent
   ],
   imports: [
     CommonModule,
-    AssessmentCrateriaRoutingModule
+    AssessmentCrateriaRoutingModule,
+    MatCardModule,
+    MatSelectModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatDialogModule,
+    MatMenuModule,
+    MatAutocompleteModule,
+    MatTooltipModule,
+    MatTableModule,
+    MatCheckboxModule,
+    MatRadioModule
   ]
 })
 export class AssessmentCrateriaModule { }
