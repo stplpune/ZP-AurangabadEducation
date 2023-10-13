2345ef7802b23ec837e117ec9a28cdac60e15dbe
deepalighume <deepali.ghume@shauryatechnosoft.com>
2023-10-13 18:01:10 +0530

    ADDED HOME IN PUBLIC

--- src/app/layout/public/public-routing.module.ts
+++ src/app/layout/public/public-routing.module.ts
@@ -4,6 +4,9 @@ import { PublicComponent } from './public.component';
 
 const routes: Routes = [
   { path: '', component: PublicComponent },
+  { path: 'home', loadChildren: () => import('../../before-login/home/home.module').then(m => m.HomeModule) },
+  // { path: 'login', loadChildren: () => import('../../before-login/login/login.module').then(m => m.LoginModule), data: { breadcrumb: [{ title: 'Login', active: true }] } },
+  // { path: 'forgot-password', loadChildren: () => import('../../before-login/forget-password/forget-password.module').then(m => m.ForgetPasswordModule), data: { breadcrumb: [{ title: 'Login', active: true }] } },
 
 ];
 
