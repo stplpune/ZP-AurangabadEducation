45ce253a3d00083f84a7a0bd9b383b2d485ae8e3
Shilpa-ShauryaTechnosoft <shilpa.ahire@shauryatechnosoft.com>
2023-10-13 15:41:19 +0530

    minor changes - login page

--- src/app/before-login/login/login.component.ts
+++ src/app/before-login/login/login.component.ts
@@ -64,7 +64,7 @@ export class LoginComponent {
   }
 
   captcha() {
-    // this.loginForm.controls['recaptchaReactive'].setValue('');
+    // this.f['captcha'].setValue('');
     this.commonMethod.createCaptchaCarrerPage();
   }
 
