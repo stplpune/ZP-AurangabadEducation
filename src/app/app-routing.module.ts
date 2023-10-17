92f397a21f54f574664585d496827a9bbabab904
Shilpa-ShauryaTechnosoft <shilpa.ahire@shauryatechnosoft.com>
2023-10-17 10:18:05 +0530

    login redirected

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,11 @@ import { AuthGuard } from './core/guards/auth.guard';
 import { checkLoggedInGuard } from './core/guards/check-logged-in.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '', canActivate: [checkLoggedInGuard], component: PublicComponent,  loadChildren: () => import('./layout/public/public.module').then(m => m.PublicModule) },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', component: PublicComponent,  loadChildren: () => import('./layout/public/public.module').then(m => m.PublicModule) },
   { path: '', canActivate:[AuthGuard], component: SecureComponent, loadChildren: () => import('./layout/secure/secure.module').then(m => m.SecureModule) },
 
-  { path: 'home', loadChildren: () => import('../../src/app/before-login/home/home.module').then(m => m.HomeModule) },
+  // { path: 'home', loadChildren: () => import('../../src/app/before-login/home/home.module').then(m => m.HomeModule) },
   { path: 'login', loadChildren: () => import('../../src/app/before-login/login/login.module').then(m => m.LoginModule), data: { breadcrumb: [{ title: 'Login', active: true }] } },
   { path: 'forgot-password', loadChildren: () => import('../../src/app/before-login/forget-password/forget-password.module').then(m => m.ForgetPasswordModule), data: { breadcrumb: [{ title: 'ForgotPassword', active: true }] } },
  
