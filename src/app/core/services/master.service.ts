4954343d514ec65d27cf56cd000b95302970eba8
Shilpa-ShauryaTechnosoft <shilpa.ahire@shauryatechnosoft.com>
2023-10-18 17:27:16 +0530

    getAllSchoolMedium added - master service

--- src/app/core/services/master.service.ts
+++ src/app/core/services/master.service.ts
@@ -79,6 +79,16 @@ export class MasterService {
     });
   }
 
+  getAllSchoolMedium(langFlag?: string) {
+    return new Observable((obj) => {
+      this.apiService.setHttp('GET', 'ZP-education/Master/GetAllSchoolMedium?flag_lang=' + langFlag, false, false, false, 'zp-Education');
+      this.apiService.getHttp().subscribe({
+        next: (res: any) => { if (res.statusCode == "200") { obj.next(res) } else { obj.error(res); } },
+        error: (e: any) => { obj.error(e) }
+      });
+    });
+  }
+
   getAllDesignationLevel(langFlag?: string) {
     return new Observable((obj) => {
       this.apiService.setHttp('GET', 'ZP-education/Master/GetAllDesignationLevel?flag_lang=' + langFlag, false, false, false, 'zp-Education');
