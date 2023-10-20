28dda0823ab77e6828fd63ab5b624ce5613c30c8
Shilpa-ShauryaTechnosoft <shilpa.ahire@shauryatechnosoft.com>
2023-10-20 11:00:09 +0530

    file upload api changed

--- src/app/core/services/file-upload.service.ts
+++ src/app/core/services/file-upload.service.ts
@@ -35,7 +35,7 @@ export class FileUploadService {
               formData.append('FolderName', folderName);
               formData.append('DocumentType', docExt);
               formData.append('UploadDocPath', file);
-              this.apiService.setHttp('post', 'ZP-Education/Document/UplodFile', false, formData, false, 'zp-Education');
+              this.apiService.setHttp('post', 'ZP-Education/Documents/UplodFile', false, formData, false, 'zp-Education');
               this.apiService.getHttp().subscribe({
                 next: (res: any) => {
                   this.spinner.hide();
@@ -97,7 +97,7 @@ export class FileUploadService {
       }
 
       if (docTypeCheckFlag == true) {
-        this.apiService.setHttp('post', 'ZP-Education/Document/upload-multiple-photos', false, formData, false, 'zp-Education');
+        this.apiService.setHttp('post', 'ZP-Education/Documents/Upload-Multiple-Files', false, formData, false, 'zp-Education');
         this.apiService.getHttp().subscribe({
           next: (res: any) => {
             if (res.statusCode == 200) {
