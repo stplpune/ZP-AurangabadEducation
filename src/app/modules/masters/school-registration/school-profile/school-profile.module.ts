eab843e659c24a8ed9dd843c46153a0902517d22
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2023-10-23 15:28:36 +0530

    attendance analysis tab designed in school profile page

--- src/app/modules/masters/school-registration/school-profile/school-profile.module.ts
+++ src/app/modules/masters/school-registration/school-profile/school-profile.module.ts
@@ -5,17 +5,27 @@ import { SchoolProfileRoutingModule } from './school-profile-routing.module';
 import { SchoolProfileComponent } from './school-profile.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { AttendanceAnalysisDetailsComponent } from './attendance-analysis-details/attendance-analysis-details.component';
 
 
 @NgModule({
   declarations: [
-    SchoolProfileComponent
+    SchoolProfileComponent,
+    AttendanceAnalysisDetailsComponent
   ],
   imports: [
     CommonModule,
     SchoolProfileRoutingModule,
     MatCardModule,
-    MatTabsModule
+    MatTabsModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule
   ]
 })
 export class SchoolProfileModule { }
