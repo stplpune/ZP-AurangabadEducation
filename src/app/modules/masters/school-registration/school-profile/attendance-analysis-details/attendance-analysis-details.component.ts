9c350b79a9cae5fedf0753e8ad4b7851e1b03661
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2023-10-23 16:08:25 +0530

    attendance analysis details page designed

--- src/app/modules/masters/school-registration/school-profile/attendance-analysis-details/attendance-analysis-details.component.ts
+++ src/app/modules/masters/school-registration/school-profile/attendance-analysis-details/attendance-analysis-details.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-attendance-analysis-details',
+  standalone: true,
+  imports: [
+    MatDialogModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   templateUrl: './attendance-analysis-details.component.html',
   styleUrls: ['./attendance-analysis-details.component.scss']
 })
