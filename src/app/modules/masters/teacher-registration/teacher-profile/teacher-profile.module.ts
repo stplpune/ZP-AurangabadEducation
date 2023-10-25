36f1fbee4c28d4fe096f3cfbf0f1060e555c64db
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2023-10-25 19:10:23 +0530

    teacher profile page designed

--- src/app/modules/masters/teacher-registration/teacher-profile/teacher-profile.module.ts
+++ src/app/modules/masters/teacher-registration/teacher-profile/teacher-profile.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { TeacherProfileRoutingModule } from './teacher-profile-routing.module';
 import { TeacherProfileComponent } from './teacher-profile.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -11,7 +13,9 @@ import { TeacherProfileComponent } from './teacher-profile.component';
   ],
   imports: [
     CommonModule,
-    TeacherProfileRoutingModule
+    TeacherProfileRoutingModule,
+    MatCardModule,
+    MatButtonModule
   ]
 })
 export class TeacherProfileModule { }
