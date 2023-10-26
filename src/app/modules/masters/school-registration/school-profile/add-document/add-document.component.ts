684819a2ac55bc784628403caac6304e9b83e7c3
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2023-10-26 18:58:10 +0530

    add document page designed

--- src/app/modules/masters/school-registration/school-profile/add-document/add-document.component.ts
+++ src/app/modules/masters/school-registration/school-profile/add-document/add-document.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+
 
 @Component({
   selector: 'app-add-document',
+  standalone: true,
+  imports: [MatDialogModule, MatButtonModule, MatIconModule, MatInputModule],
   templateUrl: './add-document.component.html',
   styleUrls: ['./add-document.component.scss']
 })
