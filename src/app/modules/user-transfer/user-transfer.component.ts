7078a82a7b576dfec322f8b824134e23a5d03ef8
stpl-skishorUI <kishor.shetti@shauryatechnosoft.com>
2023-10-26 14:54:08 +0530

    user transfer page designed

--- src/app/modules/user-transfer/user-transfer.component.ts
+++ src/app/modules/user-transfer/user-transfer.component.ts
@@ -1,7 +1,14 @@
 import { Component } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-user-transfer',
+  standalone: true,
+  imports: [MatDialogModule, MatIconModule, MatButtonModule, MatRadioModule, MatSelectModule],
   templateUrl: './user-transfer.component.html',
   styleUrls: ['./user-transfer.component.scss']
 })
