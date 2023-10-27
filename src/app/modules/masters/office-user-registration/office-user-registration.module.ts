6e7de2f89abe621da6013662d8c2cb1a949e121f
deepalighume <deepali.ghume@shauryatechnosoft.com>
2023-10-27 11:57:56 +0530

    added filter dropdown api

--- src/app/modules/masters/office-user-registration/office-user-registration.module.ts
+++ src/app/modules/masters/office-user-registration/office-user-registration.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { OfficeUserRegistrationRoutingModule } from './office-user-registration-routing.module';
 import { OfficeUserRegistrationComponent } from './office-user-registration.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
