ed5147d28270bbb30e393aafcf68c5b4f040210a
deepalighume <deepali.ghume@shauryatechnosoft.com>
2023-11-06 19:14:44 +0530

    patch officer patch

--- src/app/modules/masters/office-user-registration/add-user/add-user.component.ts
+++ src/app/modules/masters/office-user-registration/add-user/add-user.component.ts
@@ -1,8 +1,9 @@
 import { NgSwitch } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NgxSpinnerService } from 'ngx-spinner';
+import { AesencryptDecryptService } from 'src/app/core/services/aesencrypt-decrypt.service';
 import { ApiService } from 'src/app/core/services/api.service';
 import { CommonMethodService } from 'src/app/core/services/common-method.service';
 import { ErrorService } from 'src/app/core/services/error.service';
@@ -41,7 +42,8 @@ export class AddUserComponent {
   checked: boolean = false;
   age!: number;
   editObj: any;
-  // teacherClassId!: number
+  officerId: any
+  // ClassTeacherId!: number
   // assignClassId!: number
 
   get f() { return this.userRegForm.controls }
@@ -55,45 +57,55 @@ export class AddUserComponent {
     public validation: ValidationService,
     private ngxSpinner: NgxSpinnerService,
     private apiService: ApiService,
-    private router: Router) { }
+    private router: Router,
+    private route: ActivatedRoute,
+    private AESEncryptDecryptService: AesencryptDecryptService ) { }
 
   ngOnInit() {
     this.formField();
+    this.getDataFromUrl()
     this.getDesignationLevel();
     this.getDistrict();
     this.getGender();
+  }
+  getDataFromUrl(){
+    this.route.queryParams.subscribe((queryParams: any) => {
+      this.officerId = queryParams['id'];
+    });
+    let officeId = parseInt(this.AESEncryptDecryptService.decrypt(`${decodeURIComponent(this.officerId)}`));
+    officeId > 0 ? this.getUserDatabyId(officeId) :'';
   }
 
   formField() {
     this.userRegForm = this.fb.group({
       id: [0],
-      name: [''],
-      m_Name: [''],
-      mobileNo: [''],
-      emailId: [''],
+      name: [this.editObj ? this.editObj.name :''],
+      m_Name: [this.editObj ? this.editObj.m_Name : ''],
+      mobileNo: [this.editObj ? this.editObj.mobileNo : ''],
+      emailId: [this.editObj ? this.editObj.emailId :''],
       genderId: [0],
-      dob: [''],
-      address: [''],
+      dob: [this.editObj ? this.editObj.dob : ''],
+      address: [this.editObj ? this.editObj.address : ''],
       schoolId: [0], //??
       designationLevelId: [0],
       designationId: [0],
       stateId: [0],
       districtId: 0,
       talukaId: 0,
-      kendraMobileNo: [''],
-      kendraEmailId: [''],
-      beoEmailId: [''],
-      beoMobileNo: [''],
+      kendraMobileNo: [this.editObj ? this.editObj.kendraMobileNo : ''],
+      kendraEmailId: [this.editObj ? this.editObj.kendraEmailId : ''],
+      beoEmailId: [this.editObj ? this.editObj.beoEmailId : ''],
+      beoMobileNo: [this.editObj ? this.editObj.beoMobileNo : ''],
       bitId: [0],
       centerId: [0],
       agencyId: [0],
       o_UserId: [0],
       profilePhoto: [''],
-      isHeadMaster: [false],
+      isHeadMaster: [this.editObj ? this.editObj.isHeadMaster:false],
       teacherId: [0], //??
-      currentAddress: [''],
-      permanentAddress: [''],
-      isClassTeacher: [false],
+      currentAddress: [this.editObj ? this.editObj.currentAddress: ''],
+      permanentAddress: [this.editObj ? this.editObj.permanentAddress: ''],
+      isClassTeacher: [this.editObj ? this.editObj.isClassTeacher :false],
       officerCenterSchoolModel: [],
       teacherCode: [""],
       current_DistrictId: [0],
@@ -125,7 +137,7 @@ export class AddUserComponent {
       localId: [0],
       lan: this.webStorage.languageFlag,
       ...this.webStorage.createdByProps(),
-      teacherClassId: [],
+      ClassTeacherId: [],
       assignClassId: []
     })
   }
@@ -137,6 +149,8 @@ export class AddUserComponent {
     this.masterService.getAllDesignationLevel('').subscribe({
       next: (res: any) => {
         res.statusCode == "200" ? (this.levelArray = res.responseData) : this.levelArray = [];
+        this.editObj ? (this.f['designationLevelId'].setValue(this.editObj.designationLevelId), this.getDesignationByLevelId()) : '';
+
       },
     });
   }
@@ -146,6 +160,7 @@ export class AddUserComponent {
     this.masterService.getAllGender('').subscribe({
       next: (res: any) => {
         res.statusCode == "200" ? (this.genderArr = res.responseData) : this.genderArr = [];
+        this.editObj ? this.f['genderId'].setValue(this.editObj.genderId) : ''
       },
     });
   }
@@ -155,6 +170,8 @@ export class AddUserComponent {
     this.masterService.getAllDistrict('').subscribe({
       next: (res: any) => {
         res.statusCode == "200" ? this.districtArray = res.responseData : this.districtArray = [];
+        this.editObj ? (this.f['districtId'].setValue(this.editObj.districtId), this.f['t_DistrictId'].setValue(this.editObj.t_DistrictId),
+        this.getTaluka('Taluka') ): ''
       }
     });
   }
@@ -166,6 +183,7 @@ export class AddUserComponent {
       next: (res: any) => {
         res.statusCode == "200" ? this.talukaArray = res.responseData : this.talukaArray = [];
         (res.statusCode == "200" && label == 'Taluka') ? this.talukaArraySecond = res.responseData : this.talukaArraySecond = [];
+        this.editObj ? (this.f['talukaId'].setValue(this.editObj.talukaId), this.f['t_TalukaId'].setValue(this.editObj.t_TalukaId), this.getCenter('Center'), this.getBit()) :''
       }
     });
   }
@@ -176,6 +194,7 @@ export class AddUserComponent {
     this.masterService.getAllDepenDesignationByLevelId('', desigLevelId).subscribe({
       next: (res: any) => {
         res.statusCode == "200" ? this.desigLevelArray = res.responseData : this.desigLevelArray = [];
+        this.editObj ? this.f['designationId'].setValue(this.editObj.designationId) :''
       }
     });
   }
@@ -187,6 +206,23 @@ export class AddUserComponent {
       next: (res: any) => {
         res.statusCode == "200" ? this.centerArray = res.responseData : this.centerArray = [];
         (res.statusCode == "200" && label == 'Center') ? this.centerArraySecond = res.responseData : this.centerArraySecond = [];
+        this.editObj ? (this.f['t_CenterId'].setValue(this.editObj.t_CenterId),this.getVillage()):''
+        if(this.editObj){
+          let arr = this.editObj.officerCenterSchoolResponseModel
+            if (arr.length) {
+              arr.forEach((element:any) => {
+                let obj = {
+                    "id": element.centerId,
+                    "talukaId": "",
+                    "center": "",
+                    "m_Center": "",
+                    "centerCode": ""
+                  }
+                   arr.push(obj);
+              });
+              this.f['centerId'].setValue(arr);
+              }
+        }
       }
     });
   }
@@ -207,6 +243,7 @@ export class AddUserComponent {
     this.masterService.getAllVillage('', centerId).subscribe({
       next: (res: any) => {
         res.statusCode == "200" ? this.villageArray = res.responseData : this.villageArray = [];
+        this.editObj ? (this.f['t_VillageId'].setValue(this.editObj.t_VillageId), this.getAllSchool()) :''
       }
     });
   }
@@ -217,6 +254,8 @@ export class AddUserComponent {
     this.masterService.getAllSchool('', formValue.t_DistrictId, formValue.t_TalukaId, formValue.t_CenterId, formValue.t_VillageId).subscribe({
       next: (res: any) => {
         res.statusCode == "200" ? this.schoolArr = res.responseData : this.schoolArr = [];
+        this.editObj ? (this.f['t_SchoolId'].setValue(this.editObj.t_SchoolId), this.getAllSchoolClasses()) :''
+
       }
     });
   }
@@ -265,6 +304,27 @@ export class AddUserComponent {
     this.masterService.getAllSchoolClasses(schoolId, '').subscribe({
       next: (res: any) => {
         res.statusCode == "200" ? this.schoolClasArr = res.responseData : this.schoolClasArr = [];
+        if(this.editObj){
+          let arr = this.editObj.teacherClassesResponseModel
+          if (arr.length) {
+            let newArr:any =[]
+            arr.forEach((element:any) => {
+              let obj = {
+                "id": 0,
+                "teacherId": 0,
+                "standardId": element.standardId,
+                "divisionId": element.divisionId,
+                "isClassTeacher": true,
+                "createdBy": 1,
+                "isDeleted": false
+                }
+                newArr.push(obj);
+            });
+            this.f['assignClassId'].setValue(newArr);
+            console.log("this.f['assignClassId']",this.f['assignClassId'].value);
+            
+            }
+        }
       }
     });
   }
@@ -354,42 +414,61 @@ export class AddUserComponent {
   }
 
   onSubmit() {
+    let webStorageMethod = this.webStorage.createdByProps();
     let formValue = this.userRegForm.value;
-    formValue.officerCenterSchoolModel = []
-    //  Start if AssignClasses  slected by user
+    //  Start if AssignClasses for all teacher slected by user
     let assignClassData = this.userRegForm.value.assignClassId
     let assignClassArr: any = []
     assignClassData?.forEach((element: any) => {
       let obj = {
-        "id": element.id,
+        "id": 0,
         "teacherId": 0,
         "standardId": element.standardId,
         "divisionId": element.divisionId,
         "isClassTeacher": true,
-        "createdBy": 0,
-        "isDeleted": true
+        "createdBy": webStorageMethod.createdBy,
+        "isDeleted": webStorageMethod.isDeleted
       }
       assignClassArr.push(obj)
     });
     //  End AssignClasses  slected by user
 
     // Start teacherClass selected by user
-    let teacherClassData = this.userRegForm.value.teacherClassId
     let classTeacherArr: any = []
-    teacherClassData?.forEach((element: any) => {
-      let obj = {
-        "id": element.id,
-        "teacherId": 0,
-        "standardId": element.standardId,
-        "divisionId": element.divisionId,
-        "isClassTeacher": true,
-        "createdBy": 0,
-        "isDeleted": true
-      }
-      classTeacherArr.push(obj)
-    });
+    if(this.f['isClassTeacher'].value == true){
+      let classTeacherData = this.userRegForm.value.ClassTeacherId
+      classTeacherData?.forEach((element: any) => {
+        let obj = {
+          "id": element.id,
+          "teacherId": element.teacherId,
+          "standardId": element.standardId,
+          "divisionId": element.divisionId,
+          "isClassTeacher": true,
+          createdBy: webStorageMethod.createdBy,
+          isDeleted: webStorageMethod.isDeleted
+        }
+        classTeacherArr.push(obj);
+      });
+    }
+    
 
-
+    // start officerCenterModel
+    let centerArr: any = []
+    if(this.userRegForm.value.designationId == 17){
+      let centerData = this.userRegForm.value.centerId
+      console.log("centerData", centerData);
+      
+      centerData?.forEach((element:any) => {
+        let centerObj = {
+          "id": 0,
+          "userId": 0,
+          "centerId": element.id,
+          "centerSchoolId": 0,
+          "addUpdateBy": webStorageMethod.modifiedBy
+        }
+        centerArr.push(centerObj);
+      });
+    }
     let url = this.editObj ? 'UpdateOfficer' : 'AddOfficer';
     if (!this.userRegForm.valid) {
       this.commonMethod.matSnackBar(this.webStorage.languageFlag == 'EN' ? 'Please Enter Mandatory Fields' : 'कृपया अनिवार्य फील्ड प्रविष्ट करा', 1);
@@ -397,9 +476,13 @@ export class AddUserComponent {
     }
     else {
       console.log("hds", formValue);
-      formValue.classTeacherModel = assignClassArr // patch selected Assignclasses to headmaster
-      formValue.teacherClassesModel = classTeacherArr
+      // formValue.classTeacherModel = assignClassData.length ?  assignClassArr : [] // patch selected Assignclasses to headmaster
+      // formValue.teacherClassesModel = this.userRegForm.value.isClassTeacher == true ? classTeacherArr : [];
+      formValue.classTeacherModel =this.userRegForm.value.isClassTeacher == true ? classTeacherArr : []
+      formValue.teacherClassesModel =  assignClassData.length ?  assignClassArr : [] // patch selected Assignclasses to headmaster ;
 
+      formValue.officerCenterSchoolModel =  this.userRegForm.value.designationId == 17 ? centerArr : [];
+      formValue.centerId = 0;
       this.ngxSpinner.show();
       this.apiService.setHttp(this.editObj ? 'put' : 'post', 'ZP-Education/Officer/' + url, false, formValue, false, 'zp-Education');
       this.apiService.getHttp().subscribe({
@@ -415,6 +498,30 @@ export class AddUserComponent {
     }
   }
 
+  getUserDatabyId(officerId: any){
+    this.ngxSpinner.show();
+    this.apiService.setHttp('GET', 'ZP-Education/Officer/GetById?Id='+officerId, false, false, false, 'zp-Education');
+    this.apiService.getHttp().subscribe({
+      next: (res: any) => {
+        this.ngxSpinner.hide();
+        res.statusCode == "200" ? (this.editObj = res.responseData ,this.formField(),this.getGender(),
+        this.getDesignationLevel()) : this.commonMethod.checkDataType(res.statusMessage) == false ? this.errorService.handelError(res.statusCode) : this.commonMethod.matSnackBar(res.statusMessage, 1);
+      },
+      error: ((err: any) => {
+        this.ngxSpinner.hide();
+        this.commonMethod.checkDataType(err.statusMessage) == false ? this.errorService.handelError(err.statusCode) : this.commonMethod.matSnackBar(err.statusMessage, 1);
+      })
+    })
+  }
+
+  compareCenterFn(object1: any, object2: any) {
+    return object1 && object2 && object1.id === object2.id;
+  }
+
+  compareAssignClassFn(object1: any, object2: any) {
+    return object1 && object2 && object1.id === object2.id;
+  }
+
   clearDropdown(flag?: string) {
     switch (flag) {
       case 'designationLevel':
@@ -422,18 +529,23 @@ export class AddUserComponent {
         break
       case 'district':
         this.f['talukaId'].setValue('');
-        this.f['bitId'].setValue('');
+        this.f['bitId'].setValue(0);
         this.f['centerId'].setValue(0)
         this.centerArray = [];
         this.bitArray = [];
         break;
       case 'taluka':
-        this.f['bitId'].setValue('');
+        this.f['bitId'].setValue(0);
         this.f['centerId'].setValue(0)
         break;
       case 'designation':
-        this.f['bitId'].setValue('');
+        this.f['bitId'].setValue(0);
         this.f['centerId'].setValue(0);
+        this.f['beoEmailId'].setValue('');
+        this.f['beoMobileNo'].setValue('');
+        this.f['kendraMobileNo'].setValue('');
+        this.f['kendraEmailId'].setValue('');
+
         break;
       case 't_DistrictId':
         this.f['t_TalukaId'].setValue('');
@@ -441,7 +553,7 @@ export class AddUserComponent {
         this.f['t_VillageId'].setValue('');
         this.f['t_SchoolId'].setValue('');
         this.f['assignClassId'].setValue('');
-        this.f['teacherClassId'].setValue('');
+        this.f['ClassTeacherId'].setValue('');
 
         this.centerArray = [];
         this.villageArray = [];
@@ -453,7 +565,7 @@ export class AddUserComponent {
         this.f['t_VillageId'].setValue('');
         this.f['t_SchoolId'].setValue('');
         this.f['assignClassId'].setValue('');
-        this.f['teacherClassId'].setValue('');
+        this.f['ClassTeacherId'].setValue('');
         this.villageArray = [];
         this.schoolArr = [];
         this.schoolClasArr = [];
@@ -462,7 +574,7 @@ export class AddUserComponent {
         this.f['t_VillageId'].setValue('')
         this.f['t_SchoolId'].setValue('')
         this.f['assignClassId'].setValue('');
-        this.f['teacherClassId'].setValue('');
+        this.f['ClassTeacherId'].setValue('');
         this.villageArray = [];
         this.schoolArr = [];
         this.schoolClasArr = [];
@@ -470,13 +582,13 @@ export class AddUserComponent {
       case 't_VillageId':
         this.f['t_SchoolId'].setValue('')
         this.f['assignClassId'].setValue('');
-        this.f['teacherClassId'].setValue('');
+        this.f['ClassTeacherId'].setValue('');
         this.schoolArr = [];
         this.schoolClasArr = [];
         break;
       case 't_SchoolId':
         this.f['assignClassId'].setValue('');
-        this.f['teacherClassId'].setValue('');
+        this.f['ClassTeacherId'].setValue('');
         this.schoolClasArr = [];
         break;
       default:
